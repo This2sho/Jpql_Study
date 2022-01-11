027dfa01210e85832830eb87476e3c65a3672539
This2sho <zx00018@naver.com>
2022-01-11 17:28:49 +0900

    Initial commit

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,43 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
+            List<Member> resultList = query.getResultList();
+            Member singleResult = query.getSingleResult();
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("result.getUsername() = " + result.getUsername());
+
+            tx.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
