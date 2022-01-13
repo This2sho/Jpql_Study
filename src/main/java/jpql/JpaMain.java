6a1d72b2251edd7dd3c2ab3eec6873f4eb6dd030
This2sho <zx00018@naver.com>
2022-01-13 22:27:06 +0900

    Test: fetch join을 사용한 쿼리 테스트
    Distinct를 사용해서 애플리케이션상의 컬랙션 중복 제거, 컬랙션의 페치 조인에서 페이징 문제 해결 (BatchSize 사)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -13,32 +14,57 @@ public class JpaMain {
         tx.begin();
 
         try{
-            for(int i=0; i<100; i++){
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
 
             em.flush();
             em.clear();
 
-//            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+//            String query = "select distinct m from Member m join fetch m.team";
+//            List<Member> result = em.createQuery(query, Member.class)
 //                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//                //회원1, 팀A(SQL)
+//                //회원2, 팀A(1차캐시)
+//                //회원3, 팀B(SQL)
 //
-//            MemberDTO memberDTO = result.get(0);
-//            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-//            System.out.println("memberDTO.getUsername() = " + memberDTO.getAge());
-
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+//                //회원 100명 -> N + 1
+//            }
+            String query = "select t from Team t";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
-
-            System.out.println("result.size() = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
+            for (Team team : result) {
+                System.out.println("team.getName() + = " + team.getName() + " |members = " +team.getMembers());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
+
+
 
             tx.commit();
         } catch (Exception e){
